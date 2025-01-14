19a3465f373f7690a730aa7511d66c9fe603535c
Kleiton Marques <kleitonm.marques@gmail.com>
2025-01-14 14:52:44 -0300

    first commit

---
+++ src/main/java/br/com/kleitonmarques/todolistjava/TodolistjavaApplication.java
@@ -0,0 +1,13 @@
+package br.com.kleitonmarques.todolistjava;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TodolistjavaApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(TodolistjavaApplication.class, args);
+	}
+
+}
---
+++ src/test/java/br/com/kleitonmarques/todolistjava/TodolistjavaApplicationTests.java
@@ -0,0 +1,13 @@
+package br.com.kleitonmarques.todolistjava;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TodolistjavaApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
